7236109d15f43af9cf8e5a64d1edfb215bfab094
patsxy <patrick_pat@126.com>
2020-08-24 13:48:12 +0800

    Update ExcelUtils.java
    加入word 标题风格

--- ExcelUtils.java
+++ ExcelUtils.java
@@ -2071,6 +2071,67 @@ public class ExcelUtils {
         }
         return excelValidate;
     }
+     private static void addCustomHeadingStyle(XWPFDocument docxDocument, XWPFStyles styles, String strStyleId, int headingLevel, int pointSize, String hexColor) {
+
+        CTStyle ctStyle = CTStyle.Factory.newInstance();
+        ctStyle.setStyleId(strStyleId);
+
+
+        CTString styleName = CTString.Factory.newInstance();
+        styleName.setVal(strStyleId);
+        ctStyle.setName(styleName);
+
+        CTDecimalNumber indentNumber = CTDecimalNumber.Factory.newInstance();
+        indentNumber.setVal(BigInteger.valueOf(headingLevel));
+
+        // lower number > style is more prominent in the formats bar
+        ctStyle.setUiPriority(indentNumber);
+
+        CTOnOff onoffnull = CTOnOff.Factory.newInstance();
+        ctStyle.setUnhideWhenUsed(onoffnull);
+
+        // style shows up in the formats bar
+        ctStyle.setQFormat(onoffnull);
+
+        // style defines a heading of the given level
+        CTPPr ppr = CTPPr.Factory.newInstance();
+        ppr.setOutlineLvl(indentNumber);
+        ctStyle.setPPr(ppr);
+
+        XWPFStyle style = new XWPFStyle(ctStyle);
+
+        CTHpsMeasure size = CTHpsMeasure.Factory.newInstance();
+        size.setVal(new BigInteger(String.valueOf(pointSize)));
+        CTHpsMeasure size2 = CTHpsMeasure.Factory.newInstance();
+        size2.setVal(new BigInteger("24"));
+
+        CTFonts fonts = CTFonts.Factory.newInstance();
+        fonts.setAscii("Loma" );
+
+        CTRPr rpr = CTRPr.Factory.newInstance();
+        rpr.setRFonts(fonts);
+        rpr.setSz(size);
+        rpr.setSzCs(size2);
+
+        CTColor color=CTColor.Factory.newInstance();
+        color.setVal(hexToBytes(hexColor));
+        rpr.setColor(color);
+        style.getCTStyle().setRPr(rpr);
+        // is a null op if already defined
+
+        style.setType(STStyleType.PARAGRAPH);
+        styles.addStyle(style);
+
+    }
+
+    public static byte[] hexToBytes(String hexString) {
+        HexBinaryAdapter adapter = new HexBinaryAdapter();
+        byte[] bytes = adapter.unmarshal(hexString);
+        return bytes;
+    }	
+	
+	
+	
 }
 
 
